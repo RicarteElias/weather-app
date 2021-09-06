5ab7782d44d464839ea18a5c08d9e9a83ce47507
Ricarte Elias <ricarte.elias@outlook.com>
2021-09-05 22:20:51 -0300

    pequenos ajustes no datasource

--- lib/features/location/data/datasources/location_local_datasource.dart
+++ lib/features/location/data/datasources/location_local_datasource.dart
@@ -2,7 +2,7 @@ import 'package:weather_app/features/location/data/model/address_model.dart';
 
 abstract class LocationLocalDataSource {
   /// Gets the cached [AddressModel] which was gotten the last time
-  /// the user had an search a address.
+  /// the user had search a address.
   ///
   /// Throws [NoLocalDataException] if no cached data is present.
   Future<AddressModel> searchLocationFromLocal();
--- lib/features/location/data/repositories/location_repository_impl.dart
+++ lib/features/location/data/repositories/location_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:weather_app/core/errors/failure.dart';
+import 'package:weather_app/core/platform/network_info.dart';
 import 'package:weather_app/core/service/base_service.dart';
 import 'package:weather_app/features/location/data/datasources/lcoation_remote_data_source.dart';
 import 'package:weather_app/features/location/data/datasources/location_local_datasource.dart';
@@ -10,19 +11,22 @@ import 'package:weather_app/features/location/domain/repository/location_reposit
 class LocationRepositoryImpl extends BaseService implements LocationRepository {
   late LocationRemoteDataSource locationRemoteDataSource;
   late LocationLocalDataSource locationLocalDataSource;
+  final NetworkInfo? networkInfo;
 
-  // final NetworkInfo? networkInfo;
+  LocationRepositoryImpl(
+      {required this.locationRemoteDataSource,
+      required this.locationLocalDataSource,
+      required this.networkInfo})
+      : super();
 
-  LocationRepositoryImpl({
-    required this.locationRemoteDataSource,
-    required this.locationLocalDataSource,
-    // required this.networkInfo
-  }) : super();
+  // final String _baseUrl =
+  //     'https://maps.googleapis.com/maps/api/geocode/json?address=';
+  // final String _key = 'AIzaSyD_mBSsoM9iauGzzUl4wgnA42BbAs7v6Zw';
 
   @override
   Future<Either<Failure, Address?>> searchLocation(String searchText) async {
     try {
-      // networkInfo!.isConnected;
+      networkInfo!.isConnected;
       final AddressModel? address =
           await locationRemoteDataSource.searchLocation(searchText);
       locationLocalDataSource.cacheLocation(address);
